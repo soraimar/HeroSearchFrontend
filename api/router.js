f737388cbcae207a0549c632493670b5255e05dd
sbernal <soraimar.bernal@gmail.com>
2021-11-20 17:33:44 -0300

    adding BFF

---
+++ api/router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const axios = require("axios");
+let PORT = 3001
+const cors = require("cors");
+
+const API = 'https://superheroapi.com/api/4500791343373570/search/'
+
+app.use(cors())
+
+app.get("/heroes", function (req, res){
+
+    consultarherues(req.query.nameHeroes)
+        .then((data) =>{
+            res.status(200).send(data)
+        })
+})
+
+async function consultarherues(nameHeroes){
+
+    let res = await axios.get(API + nameHeroes)
+    return res.data;
+}
+
+
+app.listen(PORT, function (){
+    console.log("BFF Listo")
+})
